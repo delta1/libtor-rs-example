ee6d6f96b45d30daadb20ec42dbffed93ec529d9
Byron Hambly <bizzle@tari.com>
2021-09-08 12:12:34 +0200

    linker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use libtor::{HiddenServiceVersion, Tor, TorAddress, TorFlag};
+
 fn main() {
-    println!("Hello, world!");
+    match Tor::new()
+        .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+        .flag(TorFlag::SocksPort(19050))
+        .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+        .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+        .flag(TorFlag::HiddenServicePort(
+            TorAddress::Port(8000),
+            None.into(),
+        ))
+        .start()
+    {
+        Ok(r) => println!("{}", r),
+        Err(e) => eprintln!("{}", e),
+    }
 }
