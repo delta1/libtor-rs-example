570cccdffd88cbd9cd35f22560ccd0163b0e1b6d
Byron Hambly <bizzle@tari.com>
2021-10-11 09:45:18 +0200

    hack

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use libtor::{HiddenServiceVersion, Tor, TorAddress, TorFlag};
+use std::{thread::sleep, time::Duration};
+use tokio::{task, time};
 
-fn main() {
-    match Tor::new()
-        .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
-        .flag(TorFlag::SocksPort(19050))
-        .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
-        .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
-        .flag(TorFlag::HiddenServicePort(
-            TorAddress::Port(8000),
-            None.into(),
-        ))
-        .start()
-    {
-        Ok(r) => println!("{}", r),
-        Err(e) => eprintln!("{}", e),
+#[tokio::main]
+async fn main() {
+    task::spawn(async move {
+        match Tor::new()
+            .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+            .flag(TorFlag::SocksPort(19050))
+            .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+            .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+            .flag(TorFlag::HiddenServicePort(
+                TorAddress::Port(8000),
+                None.into(),
+            ))
+            .start()
+        {
+            Ok(r) => println!("{}", r),
+            Err(e) => eprintln!("{}", e),
+        }
+    });
+
+    loop {
+        print!(".");
+        time::sleep(Duration::from_secs(1)).await;
     }
 }
